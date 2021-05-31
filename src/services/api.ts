8e8054ebc75ac0c6d2030ac3a5f86aa0a936b870
Enos Andrade <enosads@gmail.com>
2021-05-31 13:00:15 -0300

    https

--- src/services/api.ts
+++ src/services/api.ts
@@ -2,5 +2,5 @@ import axios from "axios";
 
 export const api = axios.create({
     // baseURL:  "http://localhost:8000/api"
-    baseURL:  "http://mywallet3.herokuapp.com/api"
+    baseURL:  "https://mywallet3.herokuapp.com/api"
 })
